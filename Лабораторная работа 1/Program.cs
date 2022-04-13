bc20605e87db412fe5db58579044dba4502935df
Meowliss <103027259+Meowliss@users.noreply.github.com>
2022-04-13 12:18:02 +0300

    Update Program.cs
    add format xlsx

--- Лабораторная работа 1/Program.cs
+++ Лабораторная работа 1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,9 @@ using System.Text.Json;
 using System.Text.Unicode;
 using System.IO;
 using System.Xml.Serialization;
-
-
+using ClosedXML.Excel;
+using Microsoft.Office.Interop.Excel;
+using Range = Microsoft.Office.Interop.Excel.Range;
 
 namespace Лабораторная_работа_1
 {
@@ -29,25 +30,25 @@ namespace Лабораторная_работа_1
             PoemPattern.Name = "Спасибо, музыка, за то...";
             PoemPattern.Author = "Владимир Соколов";
             PoemPattern.Year = "1960";
-            PoemPattern.Body = "Стих 1";
+            PoemPattern.Body = "Содержание";
             elements.Add(PoemPattern);
 
             PoemPattern = new Poem();
             PoemPattern.Name = "Приход вдохновения";
             PoemPattern.Author = "Юнна Мориц";
             PoemPattern.Year = "1965";
-            PoemPattern.Body = "Стих 2";
+            PoemPattern.Body = "Содержание";
             elements.Add(PoemPattern);
 
             PoemPattern = new Poem();
             PoemPattern.Name = "Когда мне встречается в людях дурное...";
             PoemPattern.Author = "Эдуард Асадов";
             PoemPattern.Year = "1966";
-            PoemPattern.Body = "Стих 3";
+            PoemPattern.Body = "Содержание";
             elements.Add(PoemPattern);
         }
 
-         public interface Ifmtfile
+        public interface Ifmtfile
         {
             public abstract List<Poem> LoadFromFile(string filename);
             public abstract void SaveToFile(List<Poem> elements, string filename);
@@ -91,6 +92,46 @@ namespace Лабораторная_работа_1
                 }
             }
         }
+        class TXLSXfile : Ifmtfile
+        {
+            public List<Poem> LoadFromFile(string filename)
+            {
+                var elements = new List<Poem>();
+                Application excelApp = new Application();
+                Workbook excelBook = excelApp.Workbooks.Open(filename);
+                _Worksheet excelSheet = (_Worksheet)excelBook.Sheets[1];
+                Range excelRange = excelSheet.UsedRange;
+                
+                
+                for (int i = 1; i <= excelRange.Rows.Count; i++)
+                {
+                    Poem PoemPattern = new Poem();                   
+                    PoemPattern.Name = excelRange.Cells[i, 1].Value2.ToString();
+                    PoemPattern.Author = excelRange.Cells[i, 2].Value2.ToString();
+                    PoemPattern.Year = excelRange.Cells[i, 3].Value2.ToString();
+                    PoemPattern.Body = excelRange.Cells[i, 4].Value2.ToString();
+                    elements.Add(PoemPattern);
+                }
+
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);             
+                return elements;
+            }
+            public void SaveToFile(List<Poem> elements, string filename)
+            {
+                var wb = new XLWorkbook();
+                var ws = wb.Worksheets.Add("Стихи");
+                for (int i = 0; i < elements.Count; i++)
+                {
+                    ws.Cell("A" + Convert.ToString(i + 1)).Value = elements[i].Name;
+                    ws.Cell("B" + Convert.ToString(i + 1)).Value = elements[i].Author;
+                    ws.Cell("C" + Convert.ToString(i + 1)).Value = elements[i].Year;
+                    ws.Cell("D" + Convert.ToString(i + 1)).Value = elements[i].Body;
+                }
+                wb.SaveAs(filename);
+            }
+        }
+
 
         public static void PrintResilt(List<Poem> elements)
         {
@@ -103,40 +144,61 @@ namespace Лабораторная_работа_1
             }
         }
 
-        
+
 
         static void Main(string[] args)
         {
             var elements = new List<Poem>(); // создание коллекции лист с объектами poem 
 
-            CreateTestElements(elements); //создание тестовых элементов 
-
-            //elements = LoadFromFileXml(); // получение и конвертация файла xml
+            //CreateTestElements(elements); //создание тестовых элементов 
 
             //PrintResilt(elements); // вывод файла в консоль
 
-            //SaveToFileXml(elements); // сохранение файла xml
 
-            string loadFile = @"C:\Users\mikl\Desktop\Алиска\file.json";
-            string SaveFile = @"C:\Users\mikl\Desktop\Алиска\test";
+            /*
+            string choiceformat = Console.ReadLine(); // выбор формата
 
-            Ifmtfile fmtfile = GetFmt("JSON");
+            string SaveFile = @"C:\Users\79859\Desktop\coding\files\test."+ choiceformat; // место сохранения 
 
-            fmtfile.SaveToFile(elements, SaveFile);            
+            Ifmtfile savefmtfile = GetFmt(choiceformat); // создание объекта типа Ifmtfile, выделение места для типа json
+
+            savefmtfile.SaveToFile(elements, SaveFile); // сохранение файла в выбранном формате
 
             Console.ReadLine();
+            */
 
             
+            //string LoadFile = System.IO.File.ReadAllText(@"C:\Users\79859\Desktop\coding\files\filexlsx.xlsx"); // форматируемый файл
+            string getextension = Path.GetExtension(@"C:\Users\79859\Desktop\coding\files\filexlsx.xlsx").Substring(1); // определение формата файла
+            Ifmtfile loadfmtfile = GetFmt(getextension);
+            elements = loadfmtfile.LoadFromFile(@"C:\Users\79859\Desktop\coding\files\filexlsx.xlsx");           
+            PrintResilt(elements);
+            Console.ReadLine();
+
+
+            /*
+            string choiceformat = Console.ReadLine();
+            string SaveFile = @"C:\Users\79859\Desktop\coding\files\fileTEST." + choiceformat;
+            Ifmtfile savefmtfile = GetFmt(choiceformat);
+            savefmtfile.SaveToFile(elements, SaveFile);
+            Console.ReadLine();
+            */
         }
 
         public static Ifmtfile GetFmt(string fileformat)
         {
             switch (fileformat)
             {
-                case "JSON": return new TJSONfile();
-                case "XML": return new TXMLfile();
+                case "json": return new TJSONfile();
+                case "xml": return new TXMLfile();
+                case "xlsx": return new TXLSXfile();
+                //case "csv": return new TCSVfile();
                 default: return null;
             }
         }
+
+        
+
+
     }
 }
